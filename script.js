fb34450addb522539ffe1029fd39b57258f6d423
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2018-02-24 22:25:47 +0330

    feat :angry: change js

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ const logo = document.getElementById('logo');
 function minimize(arg) {
   arg.classList.remove('normal');
   arg.classList.add('minimize');
+  arg.classList.add('size');
 }
 
 function normal(arg) {
   arg.classList.add('normal');
+  arg.classList.add('logo');
   arg.classList.remove('minimize');
+  arg.classList.remove('size');
 }
 
 document.addEventListener('scroll', () => {
   if (window.scrollY === 0) {
     normal(header);
+    normal(logo);
   } else if (window.scrollY !== 0) {
     minimize(header);
+    minimize(logo);
   }
 });
