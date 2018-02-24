167c83d0de7c9e653d557804fcef0d6ba4510a09
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2018-02-24 21:27:00 +0330

    feat: add divs for login & Register

--- index.php
+++ index.php
@@ -12,6 +12,13 @@
 	?>
   <div class="container">
 
+    <div class="acc">
+         <span>ورود</span>
+    </div>
+    <div class="acc">
+        <span>ثبت نام</span>
+    </div>
+
    </div>
    </div>
     <?php
